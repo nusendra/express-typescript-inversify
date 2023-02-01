23a023524ca6587f22dacd9332ae86c599a58aa6
Nusendra Hanggarawan <admin@nusendra.com>
2023-02-01 21:21:42 +0700

    fix: graphql apollo won't load

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,5 +3,6 @@ dotenv.config();
 
 export default {
   port: process.env.PORT,
+  nodeEnv: process.env.NODE_ENV,
   mongoUrl: process.env.MONGODB_URL ?? "",
 };
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { json, urlencoded } from "express";
 import helmet from "helmet";
 import express, { Express } from "express";
 import { TYPES } from "./container/types";
+import config from "./config";
 
 @injectable()
 export class Server {
@@ -15,7 +16,12 @@ export class Server {
     this.expressServer = express();
     this.expressServer.use(json());
     this.expressServer.use(urlencoded({ extended: true }));
-    this.expressServer.use(helmet());
+    this.expressServer.use(
+      helmet({
+        contentSecurityPolicy:
+          config.nodeEnv === "production" ? undefined : false,
+      })
+    );
     this.graphqlServer.express = this.expressServer;
   }
 
